4e70395450003a070616c01302746e8368a6aa7e
Madjar Evan <evanmadjar@gmail.com>
2024-05-04 18:44:40 +0300

    feat: Register Note model in the admin panel

--- backend/apps/notes/admin.py
+++ backend/apps/notes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from common.mixins.admin import ReadOnlyFieldsAdmin
+from .models import Note
 
-# Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(ReadOnlyFieldsAdmin):
+    list_display = (
+        'title',
+        'owner',
+        'updated_at',
+        'created_at',
+        'is_active',
+    )
+    list_filter = (
+        'owner',
+    )
+    search_fields = (
+        'title',
+        'body',
+        'owner',
+    )
