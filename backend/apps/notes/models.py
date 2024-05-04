6da89cfed8c2d8c4fc102f965209530af8256eb8
Madjar Evan <evanmadjar@gmail.com>
2024-05-04 18:43:46 +0300

    feat: Implement simple Note model and run migr-ons

--- backend/apps/notes/models.py
+++ backend/apps/notes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from markdownx.models import MarkdownxField
+from common.mixins.models import BaseModel
 
-# Create your models here.
+
+class Note(BaseModel):
+    title = models.CharField(max_length=255)
+    body = MarkdownxField(
+        blank=True,
+        null=True
+    )
+    owner = models.ForeignKey(
+        to=get_user_model(),
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes'
+
+    def __str__(self) -> str:
+        return self.title
