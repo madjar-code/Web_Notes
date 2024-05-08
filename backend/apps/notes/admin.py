72c1474eaa5d5bb46caf8875e3f65eda53030e26
Madjar Evan <evanmadjar@gmail.com>
2024-05-08 19:03:40 +0300

    feat: Register `Folder` model in the admin panel

--- backend/apps/notes/admin.py
+++ backend/apps/notes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from common.mixins.admin import ReadOnlyFieldsAdmin
-from .models import Note
+from .models import Note, Folder
 
 
 @admin.register(Note)
@@ -18,5 +18,26 @@ class NoteAdmin(ReadOnlyFieldsAdmin):
     search_fields = (
         'title',
         'body',
+        'owner',
+    )
+
+
+@admin.register(Folder)
+class FolderAdmin(ReadOnlyFieldsAdmin):
+    list_display = (
+        'title',
+        'owner',
+        'parent',
+        'updated_at',
+        'created_at',
+        'is_active',
+    )
+    list_filder = (
+        'owner',
+        'parent',
+    )
+    search_fields = (
+        'title',
+        'parent__title',
         'owner',
     )
