ad470511dd787eca4b0f4bf32997af181a60de8e
Madjar Evan <evanmadjar@gmail.com>
2024-05-09 10:21:16 +0300

    chore: Move REST framework settings

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -118,16 +118,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Django Rest Framework
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
 # Django Simple JWT
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
--- backend/backend/settings/development.py
+++ backend/backend/settings/development.py
@@ -8,3 +8,13 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
