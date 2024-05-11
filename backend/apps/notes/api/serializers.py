ac7086dc733efd5385ed2c7cefaf3a757b546339
Madjar Evan <evanmadjar@gmail.com>
2024-05-11 16:24:59 +0300

    feat: Add `is_active` field filtration to API

--- backend/apps/notes/api/serializers.py
+++ backend/apps/notes/api/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 from rest_framework.serializers import (
     ModelSerializer,
     SerializerMethodField,
 )
+from drf_yasg.utils import swagger_serializer_method
 from notes.models import Note, Folder
 
 
@@ -13,8 +14,8 @@ class NoteTreeSerializer(ModelSerializer):
             'id',
             'title',
             # 'body',
-            'owner',
-            'folder',
+            # 'owner',
+            # 'folder',
             # 'created_at',
             # 'updated_at',
             # 'is_active',
@@ -39,11 +40,10 @@ class FolderTreeSerializer(ModelSerializer):
         read_only_fields = fields
 
     def get_children(self, instance: Folder):
-        nested_folders = instance.children.all()
+        nested_folders = instance.children.filter(is_active=True)
         folder_serializer = self.__class__(nested_folders, many=True)
 
-        notes = instance.notes.all()
+        notes = instance.notes.filter(is_active=True)
         note_serializer = NoteTreeSerializer(notes, many=True)
 
         return folder_serializer.data + note_serializer.data
-
