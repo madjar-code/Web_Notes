720603fdb3dbfc40c4dc83fca75aa6d0a86e10c1
Madjar Evan <evanmadjar@gmail.com>
2024-05-16 11:00:00 +0300

    feat: Implement custom context menu

---
+++ frontend/src/components/Home/ContextMenu.jsx
@@ -0,0 +1,51 @@
+import './ContextMenu.css'
+
+
+const ContextMenu = ({
+  rightClickItem,
+  positionX,
+  positionY,
+  isToggled,
+  buttons,
+  contextMenuRef
+}) => {
+  return (
+    <menu
+      style={{
+        top: positionY + 1 + 'px',
+        left: positionX + 1 + 'px'
+      }}
+      className={`context-menu ${isToggled ? 'active': ''}`}
+      ref={contextMenuRef}
+    >
+      {
+        buttons.map((button, index) => {
+          function handleClick(e) {
+            e.stopPropagation()
+            button.onClick(e, rightClickItem)
+          }
+
+          if (button.isSpacer) return <hr key={index}></hr>
+
+          return (
+            <button
+              onClick={handleClick}
+              key={index}
+              className='context-menu-button'
+            >
+              <span style={{
+                  color: button.text === 'Delete' ? '#F12727' : 'inherit'
+                }}
+              >
+                {button.text}
+              </span>
+              {button.icon}
+            </button>
+          )
+        })
+      }
+    </menu>
+  )
+}
+
+export default ContextMenu
