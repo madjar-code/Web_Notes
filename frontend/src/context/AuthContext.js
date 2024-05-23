1758e2ffb43449b2e641a22ba5b13158430a16a8
Madjar Evan <evanmadjar@gmail.com>
2024-05-23 17:11:33 +0300

    feat: Implement AuthContext for the frontend side

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,117 @@
+import {
+  createContext,
+  useState,
+  useEffect
+} from "react";
+import { useNavigate } from "react-router-dom";
+import jwt_decode from 'jwt-decode'
+
+
+const AuthContext = createContext()
+export default AuthContext
+
+
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate()
+
+  const [authTokens, setAuthTokens] = useState(
+    () => localStorage.getItem('authTokens')
+      ? JSON.parse(localStorage.getItem('authTokens'))
+      : null
+  )
+
+  const [user, setUser] = useState(
+    () => localStorage.getItem('authTokens')
+      ? jwt_decode(localStorage.getItem('authTokens'))
+      : null
+  )
+
+  const [loading, setLoading] = useState()
+
+  const loginUser = async (credentials) => {
+    const response = await fetch('/api/v1/auth/jwt/create/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+    
+    const data = await response.json()
+
+    if (response.status === 200) {
+      setAuthTokens(data)
+      setUser(jwt_decode(data.access))
+      localStorage.setItem(
+        'authTokens',
+        JSON.stringify(data)
+      )
+    }
+    return response.status
+  }
+
+  const updateToken = async () => {
+    const response = await fetch('/api/v1/auth/jwt/refresh/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'refresh': authTokens?.refresh
+      })
+    })
+
+    const data = await response.json()
+
+    if (response.status === 200) {
+      const newTokens = {
+        access: data.access,
+        refresh: authTokens.refresh
+      }
+
+      setAuthTokens(newTokens)
+      setUser(jwt_decode(newTokens.access))
+      localStorage.setItem('authTokens', JSON.stringify(newTokens))
+    } else {
+      logoutUser()
+      navigate('/login')
+    }
+
+    if (loading) {
+      setLoading(false)
+    }
+  }
+
+  const logoutUser = async () => {
+    setAuthTokens(null)
+    setUser(null)
+    localStorage.removeItem('authTokens')
+  }
+
+  const contextData = {
+    user: user,
+    authTokens: authTokens,
+    loginUser: loginUser,
+    logoutUser: logoutUser,
+  }
+
+  useEffect(() => {
+    if (loading) {
+      updateToken()
+    }
+    const accessLifeInterval = 1000 * 10 * 1
+    const interval = setInterval(() => {
+      if (authTokens) {
+        updateToken()
+      }
+    }, accessLifeInterval)
+    return () => clearInterval(interval)
+
+  }, [authTokens, loading])
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  )
+}
