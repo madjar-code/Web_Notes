53b517f69e02ed3c4bfa021ca1dcba4df86d6bd0
Madjar Evan <evanmadjar@gmail.com>
2024-05-24 10:47:33 +0300

    feat: Set default value for `is_active` is True

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -139,7 +139,7 @@ DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {
         'user_create': 'users.serializers.CustomUserCreateSerializer',
         'user': 'users.serializers.CustomUserCreateSerializer',
